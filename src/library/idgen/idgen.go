293a3785d10bf18ce4fca9c936f795d81c0b4dae
vitoorz <vitoorz@163.com>
2016-05-18 00:11:13 +0800

    add idgen unit test

--- src/library/idgen/idgen.go
+++ src/library/idgen/idgen.go
@@ -21,22 +21,28 @@ var nodeSN int = 0
 // objectIdCounter is atomically incremented when generating a new ObjectId
 var objectIDCounter uint32 = 0
 
-func InitIDGen(seed string) {
+//input string should be numbers in base 10
+func InitIDGen(seed string) bool {
 	// read seed generates machine id and puts it into the nodeSN global
 	idint, err := strconv.Atoi(seed)
-	if err != nil || idint > 0x3FFF || idint <= 0 {
+	if err != nil {
 		nodeSN = 0
-		logger.Fatal("wrong config in seed:%v", idint)
-		panic("wrong config in idgen seed")
-		return
+		logger.Fatal("wrong config in seed,Atoi fail:%v,err:%v", seed, err.Error())
+		return false
+	}
+	if idint > 0x3FFF || idint <= 0 {
+		nodeSN = 0
+		logger.Fatal("wrong config in seed range:%v", seed)
+		return false
 	}
 	logger.Info("Init idgen with seed:%d", idint)
 	nodeSN = idint
+	return true
 }
 
 func NewObjectID() (id ObjectID) {
 	if nodeSN == 0 {
-		panic("wrong config in seed")
+		logger.Fatal("something wrong in idgen seed")
 		return 0
 	}
 	currentTime := time.Now().UTC().Unix()
