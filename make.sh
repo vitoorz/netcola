06a57e6283ce72d2bb20aa75d423c74a8f2e53e9
vitoorz <vitoorz@163.com>
2017-12-04 15:07:52 +0800

    chagne make.sh to golang way

--- make.sh
+++ make.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ProjectPath=${PWD}
-#export GOPATH=$ProjectPath
+echo "GOPATH=$GOPATH"
 
 formatCode() {
 	gofmt -l -s -w $1
@@ -18,20 +18,21 @@ building() {
 
     [ -f $1 ] && rm $1
     printf "building $1 ..."
-    go build -i -gcflags "-N -l"
-    if [ $? -eq 0 ];then
-        printf "generate to app/..."
-        mv $1 "$ProjectPath/app"
-    fi
+    #go build -i -gcflags "-N -l"
+    go install -gcflags "-N -l"
+#    if [ $? -eq 0 ];then
+#        printf "generate to app/..."
+#        mv $1 "$ProjectPath/app"
+#    fi
 }
 
 main() {
-    echo "formating lib/service/types/tool:"
+    echo "formating lib/service/types/tool..."
     formatCode "$ProjectPath/library"
     formatCode "$ProjectPath/module"
     formatCode "$ProjectPath/service"
-    formatCode "$ProjectPath/server"
     formatCode "$ProjectPath/types"
+
     building "server" true
 }
 
